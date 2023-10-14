731c6179f814f5673d574b6fd7670bf9b7eca9cc
manoharmarandi <manohar.marandi15@gmail.com>
2023-10-14 15:39:54 +0530

    update cicd

--- Graph Generator Microservice/app.py
+++ Graph Generator Microservice/app.py
@@ -1,14 +1,29 @@
-# app.py
-from flask import Flask
+import os
+import json
+from azure.iot.device import IoTHubDeviceClient
 
-app = Flask(__name__)
+# Create an instance of the IoT Hub device client
+device_client = IoTHubDeviceClient.create_from_connection_string(os.getenv("IOT_HUB_CONNECTION_STRING"))
 
-# Endpoint to generate a graph based on collected temperature data
-@app.route('/graph')
-def generate_graph():
-  # Logic to generate a graph based on the collected temperature data
-  # ...
-  return 'Graph generated successfully'
+def fetch_temperature_data():
+    try:
+        # Send a message to the Temperature Data Collector Microservice
+        message = {
+            "command": "fetch_temperature_data"
+        }
+        device_client.send_message_to_output(json.dumps(message), "temperatureDataOutput")
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+        # Wait for the response from the Temperature Data Collector Microservice
+        response = device_client.receive_message_on_input("temperatureDataInput")
+        if response:
+            return response.data.decode("utf-8")
+        else:
+            return "Error: No response received"
+    except Exception as e:
+        print(f"Error fetching temperature data: {e}")
+        return "Error: Failed to fetch temperature data"
+
+if __name__ == "__main__":
+    device_client.connect()
+
+    app.run(host="0.0.0.0", port=5000)
